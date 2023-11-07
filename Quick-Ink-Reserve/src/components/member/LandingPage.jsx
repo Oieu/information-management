65118b2fb4cb3c3066fc21a6c15f0e14593f83a3
Chuyaks123 <66161070+shetbricks21@users.noreply.github.com>
2023-11-07 21:31:16 +0800

    Updated the landing page and added a button for transferring between admin/member page if user is an admin.

--- Quick-Ink-Reserve/src/components/member/LandingPage.jsx
+++ Quick-Ink-Reserve/src/components/member/LandingPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./NavBar.css";
 import "./LandingPage.css";
-import logo from "../../assets/logo.png";
-import box from "../../assets/Images/box.jpg";
-import UserWrapper from "../admin/profileComponents/userComponents/UserWrapper";
-import { Link, Route, Routes } from "react-router-dom";
-import { LogoutButtonMember } from "../admin/UI/LogoutButton";
+import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../../controllers/auth/AuthContext";
-import { AdminAuthWarning } from "../../controllers/auth/AuthComponents";
+import Nav from "./Nav";
+import MainW from "./MainW";
+import Catalog from "./Catalog";
+import Location from "./Location";
+import Aboutus from "./Aboutus";
+
 
 function LandingPage({ handleLogout }) {
   const { loginStatus, user } = useAppContext();
@@ -15,147 +16,11 @@ function LandingPage({ handleLogout }) {
   return (
     <>
       <div id="home">
-        <div className="navbar">
-          <div className="w-1/4">
-            <img src={logo} alt="Logo" />
-          </div>
-          <ul className="w-1/2">
-            <li className="active">
-              <a className="active1 list" href="#home">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#catalog" className="active1 list">
-                Services
-              </a>
-            </li>
-            <li>
-              <a className="active1 list" href="#location">
-                Location
-              </a>
-            </li>
-            <li>
-              <a className="active1 list" href="#about-us">
-                About Us
-              </a>
-            </li>
-          </ul>
-          <div className="h-[100%] w-1/4">
-            <div className="h-full w-full flex justify-center items-center">
-              {loginStatus != false ? (
-                <>
-                  <UserWrapper user={user} maxWidth={"75%"} />
-                  <div className="w-1/4 flex justify-center items-center">
-                    {loginStatus === true ? (
-                      <LogoutButtonMember handleLogout={handleLogout} />
-                    ) : (
-                      <></>
-                    )}
-                  </div>
-                </>
-              ) : (
-                <div className="w-full flex justify-center items-center gap-5">
-                  <Link
-                    to={`/login`}
-                    className="w-1/4 p-3 rounded-lg bg-orange-300 text-black hover:text-white hover:border-black hover:border-2 hover:bg-orange-500 hover:translate-y-[-5px] transition-all"
-                  >
-                    Login
-                  </Link>
-                  <Link
-                    to="/signup"
-                    className="w-1/4 p-3 rounded-lg bg-green-400 text-black hover:text-white hover:border-black hover:border-2 hover:bg-green-700 hover:translate-y-[-5px] transition-all"
-                  >
-                    Register
-                  </Link>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className={`Main`}>
-            <div className="Welcome">
-                <h1>Welcome to Quick-Ink-Reserve</h1>
-            </div>
-        </div>
-        <div className="Catalog" id="catalog">
-          <h2>Here is what we can do for you!</h2>
-
-          <div className="Carousel">
-            <div className="serv1">
-              <a target="" href="">
-                <img src={box} alt="Box making" width="600" height="400" />
-              </a>
-              <div className="Des">Box Making</div>
-            </div>
-
-            <div className="serv1">
-              <a target="" href="">
-                <img src={box} alt="Box making" width="600" height="400" />
-              </a>
-              <div className="Des">Box Making</div>
-            </div>
-
-            <div className="serv1">
-              <a target="" href="">
-                <img src={box} alt="Box making" width="600" height="400" />
-              </a>
-              <div className="Des">Box Making</div>
-            </div>
-
-            <div className="serv1">
-              <a target="" href="">
-                <img src={box} alt="Box making" width="600" height="400" />
-              </a>
-              <div className="Des">Box Making</div>
-            </div>
-
-            <div className="serv1">
-              <a target="" href="">
-                <img src={box} alt="Box making" width="600" height="400" />
-              </a>
-              <div className="Des">Box Making</div>
-            </div>
-
-            <div className="serv1">
-              <a target="" href="">
-                <img src={box} alt="Box making" width="600" height="400" />
-              </a>
-              <div className="Des">Box Making</div>
-            </div>
-
-            <div className="serv1">
-              <a target="" href="">
-                <img src={box} alt="Box making" width="600" height="400" />
-              </a>
-              <div className="Des">Box Making</div>
-            </div>
-
-            <div className="serv1">
-              <a target="" href="">
-                <img src={box} alt="Box making" width="600" height="400" />
-              </a>
-              <div className="Des">Box Making</div>
-            </div>
-          </div>
-        </div>
-
-        <div className="Loc" id="location">
-          <h2>Location</h2>
-
-          <div className="LocCont">
-            <div className="MapLoc"></div>
-
-            <div className="StoreLoc">
-              <p>Lorem Ipsum</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="About" id="about-us">
-          <h1>About us</h1>
-          <p>About Quick-Ink-Reserve Lorem Ipsum .,..,.,.</p>
-        </div>
+        <Nav handleLogout={handleLogout} user={user} loginStatus={loginStatus}/>
+        <MainW user={user} nav={nav}/>
+        <Catalog/>
+        <Location/>
+        <Aboutus/>
       </div>
     </>
   );
--- Quick-Ink-Reserve/src/components/member/Location.jsx
+++ Quick-Ink-Reserve/src/components/member/Location.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import "./LandingPage.css";
-import { FaPhone } from 'react-icons/fa';
-import { FaEnvelope } from 'react-icons/fa';
-import { FaMapMarker } from "react-icons/fa";
+import { FaMapMarker, FaEnvelope, FaPhone } from "react-icons/fa";
+import { FiMapPin } from "react-icons/fi";
 
 function Location() {
     return(
         <div className="Loc" id="location">
-        <h2>Location</h2>
+        <div className="flex items-center gap-5">
+          <FiMapPin className="text-white text-5xl" />
+          <h2 className="text-white">Location</h2>
+        </div>
 
         <div className="LocCont">
           <div className="MapLoc">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d926.1473599254754!2d123.91438337979277!3d10.351819792218485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a9996ca79d6ed1%3A0xd780b462102ea8c5!2sOptima%20Typographics!5e0!3m2!1sfil!2sph!4v1699343085069!5m2!1sfil!2sph" 
                   width="600px" 
                   height="450px" 
-                  style= {{border: '0'}}
-                  allowfullscreen="" 
+                  style= {{border: '0', borderRadius: '10px'}}
+                  allowFullScreen=""
                   loading="lazy" 
-                  referrerpolicy="no-referrer-when-downgrade">
+                  referrerPolicy="no-referrer-when-downgrade">
           </iframe>
           </div>
 
